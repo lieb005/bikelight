c8f4a826ce83c0a3f11d885fa86c43e2b4e59409
Mark <mark005@pacbell.net>
2012-08-26 09:42:34 -0700

    added some config stuff

--- PIC18_Bike_Light/bike_light.c
+++ PIC18_Bike_Light/bike_light.c
@@ -4,11 +4,9 @@
  *  Created on: Aug 25, 2012
  *      Author: mark
  */
-#include <htc.h>
+#include <xc.h>
+//#include <pic18f2550.h>
 
-#ifndef _HTC_H_
-#error "something"
-#endif
 int main(int argc, char** argv);
 void config();
 void idle_loop();
@@ -24,10 +22,47 @@ int main(int argc, char** argv)
 
 void config()
 {
+// Config stuff
+#pragma config FOSC = INTOSC_XT
+#pragma config FCMEN = ON
+#pragma config PWRT = ON
+#pragma config WDT = ON
+#pragma config PBADEN = OFF
+#pragma config MCLRE = ON
+#pragma config BOR = ON
+#pragma config LVP = OFF
 
+	GIE = 1;
+	PEIE = 1;
+	TMR2IE = 1;
+	RBIE = 1;
+
+	nRBPU = 0;
+
+	PR2 = 0xFF;
+	CCPR1L = 0b11111;
+	CCP1CON = 0;
+	TRISC = 0;
+	TRISB = 0;
+	TMR2 = 0;
+	T2CON = 0b11;
+	TMR2ON = 1;
+	CCP1CON = 0b00001111;
 }
 void idle_loop()
 {
 
+}
+
+void interrupt ISR()
+{
+	if (TMR2IF && TMR2IE)
+	{
+		TMR2IF = 0;
+	}
+	if (RBIE && RBIF)
+	{
+		RBIF = 0;
+	}
 }
 
